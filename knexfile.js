2ca92b5fc8c50e4a62f604db853d5f412f8b6069
John Daly <jcdaly97@gmail.com>
2020-05-19 13:05:11 -0700

    initial setup

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './db/users.db3'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
